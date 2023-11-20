870af263df39ba89bab36b17b1a3d222ff922988
manasatl <manasa.g365@gmail.com>
2023-11-20 16:53:44 +0530

    demogithub classadded

---
+++ src/actionclassprograms/Demogithub.java
@@ -0,0 +1,14 @@
+package actionclassprograms;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class Demogithub {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
