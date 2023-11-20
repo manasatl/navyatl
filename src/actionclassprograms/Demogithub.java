250e0fef4e9ae073e0dbd03afe2654566e35f594
manasatl <manasa.g365@gmail.com>
2023-11-20 17:01:48 +0530

    Update Demogithub.java

--- src/actionclassprograms/Demogithub.java
+++ src/actionclassprograms/Demogithub.java
@@ -1,14 +1,22 @@
 package actionclassprograms;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Demogithub {
+	
+	WebDriver driver;
   @Test
-  public void f() {
+  public void googlesearch() {
+	  driver.get("https://www.google.com/");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver=new ChromeDriver();
+	  driver.manage().window().maximize();
+	  
   }
 
 }
